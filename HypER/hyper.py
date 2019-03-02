31ed6d89056bbfe30ef309253d34e373ecc27b10
Ivana Balazevic <ivanabalazevic1002@gmail.com>
2019-03-02 12:30:36 +0000

    Update hyper.py

--- HypER/hyper.py
+++ HypER/hyper.py
@@ -187,7 +187,7 @@ if __name__ == '__main__':
     if torch.cuda.is_available:
         torch.cuda.manual_seed_all(seed)  
     d = Data(data_dir=data_dir, reverse=True)
-    experiment = Experiment(model_name, num_iterations=300, batch_size=128, learning_rate=0.001, 
+    experiment = Experiment(model_name, num_iterations=800, batch_size=128, learning_rate=0.001, 
                             decay_rate=0.99, ent_vec_dim=200, rel_vec_dim=200, cuda=True, 
                             input_dropout=0.2, hidden_dropout=0.3, feature_map_dropout=0.2,
                             in_channels=1, out_channels=32, filt_h=1, filt_w=9, label_smoothing=0.1)
